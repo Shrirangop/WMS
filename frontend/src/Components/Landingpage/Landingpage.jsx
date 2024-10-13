614a75def7a6afd7bb621595a371d3e861daef1f
shrirangop <shrirangelk221@gmail.com>
2024-10-13 11:58:25 +0530

    fixed responsiveness in home page

--- frontend/src/Components/Landingpage/Landingpage.jsx
+++ frontend/src/Components/Landingpage/Landingpage.jsx
@@ -13,14 +13,14 @@ const Landingpage = () => {
                 
                 <div className="infobox h-1/2 w-full flex flex-col justify-center items-center ">
                     <div classname="w-full h-auto border-b-4 text-right flex justify-center items-center border-b-white border-solid">
-                        <h1 className="text-5xl text-blue-500 font-500 text-left">WAREHOUSE MANAGEMENT SYSTEM</h1>
+                        <h1 className="text-center text-2xl sm:text-5xl text-blue-500 font-500 sm:text-left">WAREHOUSE MANAGEMENT SYSTEM</h1>
                        
                     </div>
 
                    
 
-                    <div className="textbox w-full h-auto flex justify-center items-center mt-8">
-                        <p className="text-blue-400 text-justify text-2xl ">
+                    <div className="textbox w-full h-auto flex justify-center items-center mt-8 mb-4">
+                        <p className="text-blue-400 text-justify text-lg sm:text-2xl ">
                             <TypeAnimation
                                 sequence={[
                                     "View Stock",
@@ -36,16 +36,16 @@ const Landingpage = () => {
                             />
                         </p>
                     </div>
-                    <div className="h-1/6 w-full mt-6 rounded-md text-white flex justify-center items-centerS">
+                    {/* <div className="h-1/6 w-full mt-6 rounded-md text-white flex justify-center items-centerS">
                         <button type="button" class="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ... h-full w-1/4 ">
                            Get Started
                         </button>
                       
-                    </div>
+                    </div> */}
 
 
                 </div>
-                <img src={vector} alt="Vector" className="h-1/2 w-1/2 mb-4" />
+                <img src={vector} alt="Vector" className="w-full h-1/2 lg:w-1/2 mb-4" />
 
             </div>
         </>
